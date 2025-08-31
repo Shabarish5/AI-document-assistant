031fd6d1ce40484ba7299dc1d46f7e0342890622
Shabarish B L <95162986+Shabarish5@users.noreply.github.com>
2025-08-31 14:52:09 +0530

    Update create_memory.py

--- create_memory.py
+++ create_memory.py
@@ -3,7 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-## Uncomment the following files if you're not using pipenv as your virtual environment manager
+
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
